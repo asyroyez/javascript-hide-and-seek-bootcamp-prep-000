2cbbf3c5f7e4304776f77b14a33ff27a830b29cf
Andrey Syroyezhkin <asyroyez@gmail.com>
2017-11-28 21:33:23 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,31 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+  return document.querySelector(`div#nested div.target`)
+}
+
+function increaseRankBy(n) {
+  const rankedLists = document.querySelectorAll('.ranked-list')
+
+  for (let a = 0; a < rankedLists.length; a++) {
+    let children = rankedLists[a].children
+
+    for (let b = 0; b < children.length; b++) {
+      children[b].innerHTML = parseInt(children[b].innerHTML) + n
+    }
+  }
+}
+
+function deepestChild() {
+  let node = document.getElementById('grand-node')
+  let nextNode = node.children[0]
+
+  while (nextNode) {
+    node = nextNode
+    nextNode = node.children[0]
+  }
+
+  return node
+}
