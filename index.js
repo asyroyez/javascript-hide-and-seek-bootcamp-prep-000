b9c1b8efa2a8072ae05ed80e060843982f6d1bc1
Andrey Syroyezhkin <asyroyez@gmail.com>
2017-11-28 20:26:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ function increaseRankBy(n) {
   const list = document.getElementById('app').querySelectorAll(`ul.unranked-list li`)
 
   for (let i = 0; i < list.length; i++) {
-    list[i].innerHTML = parseInt(n, 10);  
+    list[i].innerHTML = parseInt(n, 10) + i;  
   }
 }
